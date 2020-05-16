b12b8ce0510df8a0eddb3231b4e3a25bc30cb802
abhishekagarwal <abhishekagarwal110589@gmail.com>
2020-05-16 22:07:29 +0530

    microservices course

---
+++ AdvertApi/AdvertApi/Services/AdvertDbModel.cs
@@ -0,0 +1,29 @@
+﻿using AdvertApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace AdvertApi.Services
+{
+    [DynamoDBTable("Advert")]
+    public class AdvertDbModel
+    {
+        [DynamoDBProperty]
+        public string Title { get; set; }
+        
+        [DynamoDBProperty]
+        public string Description { get; set; }
+        
+        [DynamoDBProperty]
+        public double Price { get; set; }
+        public DateTime CreationDateTime { get; set; }
+
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+
+        [DynamoDBProperty]
+        public AdvertStatus Status { get; set; }
+    }
+}
